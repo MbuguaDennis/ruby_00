b998929cde92c533c0ca0c424df29dfdcdafeb83
MAMAL ADMIN <admin@mamal.nbi>
2024-11-20 16:19:55 +0300

    Updated ruby array files

---
+++ ruby-arrays/arrays.rb
@@ -0,0 +1,7 @@
+students = ["Cindy", "Josiah", "Rigo"]
+puts students[0]
+puts students[2]
+puts students[-1]
+puts students.length
+
+puts students.pop
---
+++ ruby-arrays/arrays1.rb
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+
+ fruits.each do |fruit|
+    puts fruit
+end
+
+fruits.push("pineapple")
+fruits.unshift("Thorn Melon")
+puts fruits
